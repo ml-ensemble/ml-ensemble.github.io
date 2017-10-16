2b6a4e1572beaf636667467eb962c2946c4e1a5f
Sebastian Flennerhag <sebastianflennerhag@hotmail.com>
2017-10-16 13:22:04 +0100

    rem mlens

--- mlens/parallel/tests/test_a_base_functions.py
+++
@@ -1,9 +0,0 @@
-"""ML-ENSEMBLE
-
-Test base functions used by sublearners
-"""
-import os
-import numpy as np
-from mlens.parallel._base_functions import slice_array,  assign_predictions
-
-# TODO: Write tests
